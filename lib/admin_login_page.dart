121246b0127cf0cf6ef8bad3c953996186f8deca
Colet-er <diannatolentino03@gmail.com>
2026-05-13 15:03:32 +0800

    asiacali

--- lib/admin_login_page.dart
+++ lib/admin_login_page.dart
@@ -16,7 +16,7 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
 
   // For demo: hardcoded admin credentials
   final String _adminUser = 'admin';
-  final String _adminPass = 'admin1234';
+  final String _adminPass = 'admin123;
 
   void _login() async {
     setState(() {
